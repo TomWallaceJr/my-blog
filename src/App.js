6cd22c1225f063a3b59d4c43d509fb5a5eb45f5e
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-03-08 18:46:54 -0500

    React Router installed and Routes set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import SinglePost from './Components/SinglePost';
+import Post from './Components/Post';
+import Portfolio from './Components/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={Post} path='/post' />
+        <Route component={Portfolio} path='/portfolio' />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
