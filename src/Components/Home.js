d0ea21743c24382ae2c40006e4f74d0ec6fd1135
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-03-08 20:20:14 -0500

    writes code for Post Component using hooks, GROQ, and tailwind CSS

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import image from '../images/earth.jpg';
 
 export default function Home() {
-    return <h1>Home Page</h1>
+    return (
+        <main>
+            <img src={image} alt="earth" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-4xl text-green-200 font-bold cursive home-name">Hello! I'm Tom</h1>
+            </section>
+        </main>
+    )
 }
